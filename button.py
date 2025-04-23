fa049948d875e0ab4578a64f04bd3bd4efeb9c8c
Alexr360 <alexr000360@gmail.com>
2025-04-22 22:16:47 -0600

    Enhance button detection script to specify button actions for each pin

--- button.py
+++ button.py
@@ -3,7 +3,10 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 18 to be an input pin and set initial value to be pulled low (off)
 
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
+        print("Button on pin 10 was pushed!")
+    if GPIO.input(18) == GPIO.HIGH:
+        print("Button on pin 18 was pushed!")

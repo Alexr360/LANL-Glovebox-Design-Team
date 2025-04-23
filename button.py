355021d3568ad1402673212ba2a64c07775fb315
Alexr360 <alexr000360@gmail.com>
2025-04-22 22:24:43 -0600

    Refactor button pin configuration to use BOARD numbering and set pull-down resistors

--- button.py
+++ button.py
@@ -1,17 +1,17 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+import time  # Import time library for delays
 
 # Define GPIO pins for buttons
-button1 = 17
-button2 = 27
+button1 = 11
+button2 = 23
 
-GPIO.setmode(GPIO.BCM)  # Use Broadcom pin numbering
-GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Enable internal pull-up
-GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Enable internal pull-up
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
+GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Set pin 18 to be an input pin and set initial value to be pulled low (off)
 
-while True:
-    button1_status = "HIGH" if GPIO.input(button1) == GPIO.HIGH else "LOW"
-    button2_status = "HIGH" if GPIO.input(button2) == GPIO.HIGH else "LOW"
-
-    print(f"Button 1: {button1_status}, Button 2: {button2_status}")
+while True:  # Run forever
+    pin_10_status = "HIGH" if GPIO.input(button1) == GPIO.HIGH else "LOW"
+    pin_18_status = "HIGH" if GPIO.input(button2) == GPIO.HIGH else "LOW"
+    print(f"Button 1: {pin_10_status}, Button 2: {pin_18_status}")
     time.sleep(0.5)  # Wait for 1/2 second

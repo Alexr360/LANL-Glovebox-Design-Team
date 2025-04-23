7cd4fbcdeea5e393290692e548b474fc2106153e
Alexr360 <alexr000360@gmail.com>
2025-04-22 22:15:15 -0600

    Refactor button detection script to simplify GPIO setup and remove unused button handling

--- button.py
+++ button.py
@@ -1,38 +1,9 @@
-import RPi.GPIO as GPIO
-import time
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-print("Button Press Detection Script")
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-# Use Broadcom pin-numbering scheme
-GPIO.setmode(GPIO.BCM)
-
-# Define the GPIO pins your buttons are connected to
-BUTTON1_PIN = 17  # e.g. physical pin 11
-BUTTON2_PIN = 27  # e.g. physical pin 13
-
-# Set up each pin as an input with an internal pull-down resistor
-GPIO.setup(BUTTON1_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(BUTTON2_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# Callback functions for each button
-def button1_callback(channel):
-    print("Button 1 pressed!")
-
-def button2_callback(channel):
-    print("Button 2 pressed!")
-
-# Add event detection on rising edge (button press)
-GPIO.add_event_detect(BUTTON1_PIN, GPIO.RISING, callback=button1_callback, bouncetime=200)
-GPIO.add_event_detect(BUTTON2_PIN, GPIO.RISING, callback=button2_callback, bouncetime=200)
-
-try:
-    print("Waiting for button presses. Press Ctrl+C to exit.")
-    # Keep the script running
-    while True:
-        time.sleep(0.1)
-
-except KeyboardInterrupt:
-    print("\nExiting cleanly.")
-
-finally:
-    GPIO.cleanup()
+while True: # Run forever
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed!")

d8c46bde6f083e42dc3607ff83524c78c7941a29
Alexr360 <70153720+Alexr360@users.noreply.github.com>
2025-04-24 15:42:43 -0600

    Update lift.py

--- lift.py
+++ lift.py
@@ -8,7 +8,7 @@ BUTTON_CCW = 10
 # GPIO pin for clockwise button
 BUTTON_CW = 11
 # Motor drive speed
-DRIVE_SPEED = 0.5
+DRIVE_SPEED = 1.5
 # Motor acceleration rate
 ACCELERATION = 0.25
 # Motor deceleration rate
